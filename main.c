25cc570d08186fcc44a37a818c57c2ac12ce58a2
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-10-21 19:35:16 -0430

    main.c: added reshape function. Scaling view. Added display 	mode.

--- main.c
+++ main.c
@@ -13,11 +13,16 @@ init ()
 void
 display ()
 {
+  glColor3f (1.0, 1.0, 1.0);
+  glLoadIdentity ();
+  glScalef (1.0, 1.0, 1.0);
+  glutWireCube (1.0);
+  glFlush ();
   
 }
 
 void 
-resizeWindow (int w, int h)
+reshape (int w, int h)
 {
   float aspectratio;
   
@@ -26,10 +31,10 @@ resizeWindow (int w, int h)
       h = 1;
     }
 
-  glViewport (0, 0, w, h);
+  glViewport (0, 0, (GLsizei) w, (GLsizei) h);
   glMatrixMode (GL_PROJECTION);
   glLoadIdentity ();
-
+  glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0 );
   aspectratio = (float) w / (float) h;
 
   if (w <= h) 
@@ -40,6 +45,7 @@ resizeWindow (int w, int h)
     {
       glOrtho (-10*aspectratio, 10*aspectratio, -10, 10, 1.0, -1.0);
     }
+  glMatrixMode (GL_MODELVIEW);
   return;
 }
 
@@ -47,12 +53,14 @@ int
 main (int argc, char** argv) 
 {
   glutInit(&argc, argv);
+  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize (900, 900);
-  glutInitWindowPosition (0, 0);
-  glutCreateWindow ("Breakout 3D");
+  glutInitWindowPosition (200, 200);
+  glutCreateWindow (argv[0]);
   init (); // Limpia colores en ventana
   glutDisplayFunc (display);
-  glutReshapeFunc (resizeWindow);
+  glutReshapeFunc (reshape);
   
   glutMainLoop ();
+  return 0;
 }
