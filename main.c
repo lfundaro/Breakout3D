046e74eb4460b97c8e52b5904271c84377e76526
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-10-21 23:16:40 -0430

    main.c: redefine main.

--- main.c
+++ main.c
@@ -1,66 +1,50 @@
-#include <stdio.h>
-#include <math.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
 
-void
-init ()
+void init(void)
 {
-  glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
-  glClear (GL_COLOR_BUFFER_BIT);
-  return;
+  glClearColor (0.0, 0.0, 0.0, 0.0);
+  glShadeModel (GL_FLAT);
 }
 
-void
-display ()
+void display(void)
 {
+  glClear (GL_COLOR_BUFFER_BIT);
   glColor3f (1.0, 1.0, 1.0);
   glLoadIdentity ();
-  glScalef (1.0, 1.0, 1.0);
+  /* clear the matrix */
+  /* viewing transformation */
+  gluLookAt (0.0,-8.0, 13.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  glScalef (20.0, 20.0, 0.8);
+  glRotatef (-95.0, 1.0, 0.0, 0.0); 
+  /* modeling transformation */
   glutWireCube (1.0);
   glFlush ();
-  
 }
 
-void 
-reshape (int w, int h)
+void reshape (int w, int h)
 {
   float aspectratio;
-  
-  if (h == 0) 
-    {
-      h = 1;
-    }
-
+  aspectratio = (float) w / (float) h;
   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
   glMatrixMode (GL_PROJECTION);
   glLoadIdentity ();
-  glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0 );
-  aspectratio = (float) w / (float) h;
-
-  if (w <= h) 
-    {
-      glOrtho (-10, 10, -10/aspectratio, 10/aspectratio, 1.0, -1.0);
-    }
-  else 
-    {
-      glOrtho (-10*aspectratio, 10*aspectratio, -10, 10, 1.0, -1.0);
-    }
+  gluPerspective(100.0f, aspectratio, 1.0, 400.0);
   glMatrixMode (GL_MODELVIEW);
-  return;
 }
 
-int 
-main (int argc, char** argv) 
+int main(int argc, char** argv)
 {
   glutInit(&argc, argv);
   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize (900, 900);
-  glutInitWindowPosition (200, 200);
+  glutInitWindowSize (700, 700);
+  glutInitWindowPosition (100, 150);
   glutCreateWindow (argv[0]);
-  init (); // Limpia colores en ventana
-  glutDisplayFunc (display);
-  glutReshapeFunc (reshape);
-  
-  glutMainLoop ();
+  init ();
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutMainLoop();
   return 0;
 }
+
